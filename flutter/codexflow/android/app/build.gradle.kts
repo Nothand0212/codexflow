820a8ec3c09fa01b9cfc3bff4fbd188de8b48897
Nohtand <Nothand0212@users.noreply.github.com>
2026-05-01 16:29:41 +0800

    build: raise android min sdk for plugins

--- flutter/codexflow/android/app/build.gradle.kts
+++ flutter/codexflow/android/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.codexflow_flutter"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
